cf9146c45bdcd52b7f7cf2e0c4dfa8172ba27ac4
cdionizio <croberto123@gmail.com>
2024-10-09 11:36:43 -0300

    Resolvendo desafio Listatarefa
    Signed-off-by: cdionizio <croberto123@gmail.com>

---
+++ src/main/java/List/Operacoesbasicas/CarrinhoDeCompras.java
@@ -0,0 +1,4 @@
+package main.java.List.Operacoesbasicas;
+
+public class CarrinhoDeCompras {
+}
---
+++ src/main/java/List/Operacoesbasicas/Tarefa.java
@@ -0,0 +1,23 @@
+package main.java.List.Operacoesbasicas;
+
+public class Tarefa {
+
+    //atributo
+
+    private String descricao;
+
+    public Tarefa(String descricao) {
+
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+
+}
